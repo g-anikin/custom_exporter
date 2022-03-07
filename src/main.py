2ca03ed87fa18aa11f0c44919ccb96ff5e242e96
g-anikin <53080894+g-anikin@users.noreply.github.com>
2022-03-07 17:49:51 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from prometheus_client import make_wsgi_app
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 from wsgiref.simple_server import make_server
 from functions import get_status_jenkins, get_status_ghe, get_status_sast, get_status_nexus, get_status_artifactory
+from ssl_checker_trust import check_cert
+from get_sast_queue import sast_queue_len
 
 class DsoCollector(object):
     def __init__(self):
@@ -13,16 +15,24 @@ class DsoCollector(object):
         gauge_artifactory_service = GaugeMetricFamily('artifactory_service_status', 'artifactory_service_status', labels=['service_name'])
         gauge_artifactory_node = GaugeMetricFamily('artifactory_node_status', 'artifactory_node_status', labels=['node_name'])
         gauge_artifactory_check_page = GaugeMetricFamily('artifactory_check_page_status', 'artifactory_check_page_status', labels=['domain'])
+        gauge_cert_duration_check = GaugeMetricFamily('dso_service_cert_duration_check', 'dso_service_cert_duration_check', labels=['domain'])
+        gauge_sans_check = GaugeMetricFamily('dso_service_sans_check', 'dso_service_sans_check', labels=['domain'])
+        gauge_sast_queue_len = GaugeMetricFamily('sast_queue_len', 'sast_queue_len', labels=['domain'])
         get_status_jenkins(gauge_dso_service)
         get_status_ghe(gauge_dso_service)
         get_status_sast(gauge_dso_service)
         get_status_nexus(gauge_dso_service,gauge_nexus_service)
         get_status_artifactory(gauge_dso_service, gauge_artifactory_service, gauge_artifactory_node, gauge_artifactory_check_page)
+        check_cert(gauge_cert_duration_check, gauge_sans_check)
+        sast_queue_len(gauge_sast_queue_len)
         yield gauge_dso_service
         yield gauge_nexus_service
         yield gauge_artifactory_service
         yield gauge_artifactory_node
         yield gauge_artifactory_check_page
+        yield gauge_cert_duration_check
+        yield gauge_sans_check
+        yield gauge_sast_queue_len
 
 
 if __name__ == '__main__':
